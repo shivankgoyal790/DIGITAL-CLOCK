2a3237ebf0c99cc439c558f28c657bd78e1fd520
shivank goyal <goyal.shivank790@gmail.com>
2021-04-20 17:41:43 +0530

    npm files

---
+++ development/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Gettime from "E:/react_practice/development/src/DIGITAL-CLOCK/Gettime.js";
+ReactDOM.render(<Gettime/>,
+  document.getElementById('root')
+);
+
+
